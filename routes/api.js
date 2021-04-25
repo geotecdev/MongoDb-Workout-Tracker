c16552611a32035e219f1ac3158f03de0bb47acd
geotecdev <mikeruanedev@gmail.com>
2021-04-25 12:55:52 -0400

    add api.js file with dependencies and module export for api route

---
+++ routes/api.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const { builtinModules } = require("node:module");
+const workout = require("../models/workout");
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,26 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
-const router = require("./routes");
-
 const PORT = process.env.PORT || 3001
 
 const app = express();
-app.use(morgan("dev"));
-app.use(express.urlenconded({ extended: true }));
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 //mongose connection
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout" {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useCreateIndex: true
 });
 
 //routes
-
+app.use(require("./routes/api"));
+app.use(require("./routes/html"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`)
