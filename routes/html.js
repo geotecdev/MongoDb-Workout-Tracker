c00ccd59a29e3de37b30af133c576263d7526b2a
geotecdev <mikeruanedev@gmail.com>
2021-04-25 13:26:21 -0400

    html.js add route for landing page

--- routes/html.js
+++ routes/html.js
@@ -2,5 +2,14 @@ const express = require("express");
 const router = require("express").Router();
 const path = require("path");
 
+//index.html route
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+//exersise.html route
+
+//stats.html route
+
 
 module.exports = router;
